3c1a6b8fdad1aaaf489333af8caffebb86ff2080
Yonis Sheekh <yonis.sheekh01@stclairconnect.ca>
2019-11-10 02:31:25 -0500

    fixed #7 added the splash screen and it waits for 2 seconds than it will switch to the home page

---
+++ glassio/app/src/main/java/com/example/glassio/Splash.java
@@ -0,0 +1,27 @@
+package com.example.glassio;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    protected int _splashTime = 2000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000);
+
+    }
+}
