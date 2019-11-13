058827204255ab1c84eb14bafcb2a1c4e69dc94f
Yonis Sheekh <yonis.sheekh01@stclairconnect.ca>
2019-11-13 01:05:03 -0500

    list is about complete now you can click a button and it would sort by price from lowest to highest

--- glassio/app/src/main/java/com/example/glassio/topSells.java
+++ glassio/app/src/main/java/com/example/glassio/topSells.java
@@ -12,11 +12,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.sql.BatchUpdateException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 
 /**
@@ -24,8 +30,13 @@ import java.util.ArrayList;
  */
 public class topSells extends Fragment {
 
+    private Button priceBtn;
+    private CustomAdapter adapter;
+    private ArrayList<ListViewData> data;
 
-    ListView listView;
+
+
+    private ArrayList<ListViewData> listoItems;
     public topSells() {
     }
 
@@ -36,22 +47,42 @@ public class topSells extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_top_sells, container, false);
 
-        listView = view.findViewById(R.id.list);
+        ListView listView = view.findViewById(R.id.list);
+        listView.hasExplicitFocusable();
+        // A LIST OF THE GLASSES NEEDED
+       data = new ArrayList<>();
+
+        data.add(new ListViewData("Pinkus Stylush", R.drawable.glassesone, 70.99));
+        data.add(new ListViewData("Shades IV", R.drawable.glasseseight, 60.99));
+        data.add(new ListViewData("name 3 ", R.drawable.glassesseven, 200.69));
+        data.add(new ListViewData("name 4 ", R.drawable.glassesfive, 420.69));
+        data.add(new ListViewData("name 5", R.drawable.glassesfour, 25.99));
+        data.add(new ListViewData("name 6", R.drawable.glassesten, 40.99));
+        data.add(new ListViewData("name 7", R.drawable.glasseseleven, 100.99));
+        data.add(new ListViewData("name 8", R.drawable.glassestwo, 55.99));
+        data.add(new ListViewData("name 9", R.drawable.glassesthree, 100.99));
+        data.add(new ListViewData("name 10", R.drawable.glassestwelve, 40.75));
+        data.add(new ListViewData("name 11", R.drawable.glassesnine, 55.99));
 
 
-        // A LIST OF THE GLASSES NEEDED FOR PRICE FRAGMENT
-        final ArrayList<ListViewData> data = new ArrayList<>();
-        data.add(new ListViewData("Pinkus Perfection", R.drawable.glassesseven, 100.99));
-        data.add(new ListViewData("Shades IV", R.drawable.glassesone, 70.99));
+        priceBtn = view.findViewById(R.id.sort);
+        priceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sortArrayList();
+            }
+        });
+
 
         //CONNECTING  THE LIST VIEW TO THE CUSTOM VIEW ADAPTER THAT WILL BE MADE BELLOW
-        CustomAdapter adapter =
-                new CustomAdapter(getContext(), data);
+         adapter = new CustomAdapter(getContext(), data);
         listView.setAdapter(adapter);
-
 
         return view;
     }
+
+
+
 
     //CREATING A CUSTOM VIEW ADAPTER FOR THE LIST OF GLASSES
 
@@ -79,5 +110,21 @@ public class topSells extends Fragment {
             return  convertView;
         }
     }
+
+
+    public void sortArrayList(){
+        Collections.sort(data, new Comparator<ListViewData>() {
+            @Override
+            public int compare(ListViewData o1, ListViewData t1) {
+                return o1.getPrice().compareTo(t1.getPrice());
+            }
+
+        });
+
+
+        adapter.notifyDataSetChanged();
+    }
+
+
 
 }
