d27af51d50adce5e2efd6427fd705ed8c306038c
SaadMAmjad <0730752@myscc.ca>
2019-11-23 20:07:53 -0500

    #24 renamed suggestion to colorBlindTest

--- glassio/app/src/main/java/com/example/glassio/colorBlindTest.java
+++ glassio/app/src/main/java/com/example/glassio/colorBlindTest.java
@@ -13,10 +13,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class suggestions extends Fragment {
+public class colorBlindTest extends Fragment {
 
 
-    public suggestions() {
+    public colorBlindTest() {
         // Required empty public constructor
     }
 
@@ -25,7 +25,7 @@ public class suggestions extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_suggestions, container, false);
+        return inflater.inflate(R.layout.fragment_color_blind_test, container, false);
     }
 
 }
